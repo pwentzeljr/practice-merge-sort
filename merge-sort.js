05d5a0e8f4e1c5024019cf4e967cadbede0cf39d
Paul Wentzel <paul.wentzel@gmail.com>
2024-06-19 13:32:06 -0400

    tests passing

--- merge-sort.js
+++ merge-sort.js
@@ -4,14 +4,16 @@ function mergeSort(arr) {
 
   // Check if the input is length 1 or less
     // If so, it's already sorted: return
+  if (arr.length <= 1) return arr;
 
   // Divide the array in half
-
+  const mid = Math.floor(arr.length / 2);
   // Recursively sort the left half
+  const left = mergeSort(arr.slice(0, mid));
   // Recursively sort the right half
-
+  const right = mergeSort(arr.slice(mid));
   // Merge the halves together and return
-
+  return merge(left, right);
 }
 
 
@@ -19,6 +21,7 @@ function mergeSort(arr) {
 function merge(arrA, arrB) {
 
   // Create an empty return array
+  const rv = [];
 
   // Point to the first value of each array
   // While there are still values in each array...
@@ -26,9 +29,27 @@ function merge(arrA, arrB) {
     // Add the smaller value to the return array
     // Move the pointer to the next value in that array
 
-  // Return the return array
+  let aIdx = 0;
+  let bIdx = 0;
 
+  while (aIdx < arrA.length && bIdx < arrB.length) {
+    if (arrA[aIdx] < arrB[bIdx]) {
+      rv.push(arrA[aIdx]);
+      aIdx++;
+    } else {
+      rv.push(arrB[bIdx]);
+      bIdx++
+    }
+  }
+
+  if (aIdx < arrA.length) {
+    rv.push(...arrA.slice(aIdx));
+  } else {
+    rv.push(...arrB.slice(bIdx))
+  }
+
+  // Return the return array
+  return rv;
 }
 
 module.exports = [merge, mergeSort];
-
